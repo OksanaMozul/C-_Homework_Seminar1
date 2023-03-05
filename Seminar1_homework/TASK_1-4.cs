8f1f9fa3cb82662197cd5ab94fafe71d4cc801fd
Oksana <oksana-k83@mail.ru>
2023-03-05 13:50:22 +0300

    Добавлены задачf 4  семинара_1

--- Seminar1_homework/TASK_1-4.cs
+++ Seminar1_homework/TASK_1-4.cs
@@ -65,16 +65,16 @@
 // // Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
-{
-        int n = 1;
-        Console.Write("Введите число: ");
-        int m = int.Parse(Console.ReadLine());
-        for (int i = n; i <= m; i++)
-        {
-            if (i % 2 == 0) 
-            {
-                Console.Write(i + " ");
-            }
-        }
-        Console.ReadKey();
-    }
+// {
+//         int n = 1;
+//         Console.Write("Введите число: ");
+//         int m = int.Parse(Console.ReadLine());
+//         for (int i = n; i <= m; i++)
+//         {
+//             if (i % 2 == 0) 
+//             {
+//                 Console.Write(i + " ");
+//             }
+//         }
+//         Console.ReadKey();
+//     }
